e3d3886da2a30d4712069650f0ecd07c7f581d98
Steve Vance <steve.vance@hotmail.co.uk>
2020-03-18 12:23:54 +0000

    integration test 2 and 3 compiled and passing

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -24,4 +24,26 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('25')
   });
 
+  //Test 2 - Do the arithmetical operations update the display with the result of the operation?
+  it('should update the display with result of arithmetical operations', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('5')
+  });
+
+  //Test 3 Can multiple operations be chained together?
+  it('should be able to chain multiple operations together', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#operator_add')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+
+  });
+
+
 });
